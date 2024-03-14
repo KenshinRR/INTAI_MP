4e1d68cfefa74d7cc4f4fa76ecdd9bec47021296
KenshinRR <krreblando15@gmail.com>
2024-03-14 17:24:47 +0800

    Added Bases and GameVariables singleton
    Added GameVariables singleton wherein all the player and enemy kills and bases destroyed will be placed in.
    
    Added bases wherein if the Player or the Enemy goes over it, there score will increase in the GameVariables

---
+++ Base.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+@export var base_owner: String
+var is_destroyed = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	if(base_owner == "Enemy" && 
+	body.name == "Player" 
+	&& !is_destroyed):
+		GameVariables.player_score+= 1
+		is_destroyed = true
+	elif (base_owner == "Player" && 
+	body.name == "Enemy" 
+	&& !is_destroyed):
+		GameVariables.enemy_score+= 1
+		is_destroyed = true
+	pass # Replace with function body.
---
+++ Model/Singletons/game_variables.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var player_score = 0
+var enemy_score = 0
+
+var player_kills = 0
+var enemy_kills = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if player_score >= 3:
+		print("Player Wins!")
+	pass
