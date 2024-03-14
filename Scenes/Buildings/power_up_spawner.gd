3709c74d40b480e6e3c329d2b64f525a9c581e3c
KenshinRR <krreblando15@gmail.com>
2024-03-14 17:03:26 +0800

    Added Power Up Spawner
    I added a power up Spawner that will spawn a power up scene every 1 second. The time interval will be changed

--- Scenes/Buildings/power_up_spawner.gd
+++ Scenes/Buildings/power_up_spawner.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
+@onready var power_up_scene = preload("res://Scenes/Power Ups/power_up.tscn")
 
+var power_up
+var powere_up_position: Vector2
+
+var is_spawned = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +16,16 @@ func _ready():
 func _process(delta):
 	pass
 
+func _free_slot():
+	is_spawned = false
+	$Timer.start()
+	pass
 
 func _on_timer_timeout():
+	if !is_spawned:
+		power_up = power_up_scene.instantiate()
+		get_tree().root.add_child(power_up)
+		power_up.global_position = position
+		power_up.caught.connect(_free_slot)
+		is_spawned = true
 	pass # Replace with function body.
