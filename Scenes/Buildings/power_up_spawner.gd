52f72938f6043648492b39b24eecfdf378a4dd08
KenshinRR <krreblando15@gmail.com>
2024-03-28 19:39:41 +0800

    Fixed power up spawner
    it now randomly spawns inside player area and not on top of base and obstacles
    Also, respawn time is randomized

--- Scenes/Buildings/power_up_spawner.gd
+++ Scenes/Buildings/power_up_spawner.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 @onready var power_up_scene = preload("res://Scenes/Power Ups/power_up.tscn")
-@onready var tile_map = $"../../Map"
+var tile_map
+var astar_grid
 
+@onready var timer = $Timer
 
 var power_up
 var powere_up_position: Vector2
@@ -11,11 +13,24 @@ var is_spawned = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#setting up the variables
+	tile_map = get_tree().get_first_node_in_group("Map")
+	
+	#preparing the A* tilemap
+	astar_grid = AStarGrid2D.new()
+	astar_grid.region = tile_map.get_used_rect()
+	astar_grid.cell_size = Vector2(16,16)
+	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ALWAYS
+	astar_grid.update()
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	if is_spawned:
+		timer.wait_time = randi_range(15,20)
+		is_spawned = false
 	pass
 
 func _free_slot():
@@ -24,24 +39,29 @@ func _free_slot():
 	pass
 
 func _on_timer_timeout():
+	is_spawned = true
 	var valid = false
+	var region_size = astar_grid.region.size
+	var generated_pos : Vector2
+	
 	power_up = power_up_scene.instantiate()
 	get_tree().root.add_child(power_up)
 	
-	#while(!valid):
-	power_up.global_position.x = randf_range(52,252)
-	power_up.global_position.y = randf_range(44,204)
-	#	var tileData = tile_map.get_cell_tile_data(0, power_up.global_position)
-	#	if tileData == null or not tileData.get_custom_data("Enemy Base"):
-	#		valid = false;
-	#		continue
-	#	tileData = tile_map.get_cell_tile_data(1, power_up.global_position)
-	#	if tileData == null or not tileData.get_custom_data("Enemy Base"):
-	#		valid = false
-	#		continue
-	#	valid = true
-			
+	while(!valid):
+		generated_pos.x = randi_range(0,region_size.x)
+		generated_pos.y = randi_range(0,region_size.y)
+		var tileData = tile_map.get_cell_tile_data(0, generated_pos)
+		if tileData == null:
+			continue
+		if not tileData.get_custom_data("Walkable"):
+			valid = false;
+			continue
+		tileData = tile_map.get_cell_tile_data(1, generated_pos)
+		if tileData != null:
+			valid = false
+			continue
+		valid = true
 		
+	power_up.global_position = tile_map.map_to_local(generated_pos)
 	
-		
 	pass # Replace with function body.
